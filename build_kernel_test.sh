a472cb9c1600ee7fbd1c97d20b5c778133f98923
HRTKernel <support@hacker-kernel.com>
2016-02-25 21:17:38 +0100

    SM-G925F_MM_Opensource_G925FXXU3DPB8

--- build_kernel_test.sh
+++ build_kernel_test.sh
@@ -3,5 +3,5 @@
 export ARCH=arm64
 export CROSS_COMPILE=/opt/toolchains/aarch64-linux-android-4.8/bin/aarch64-linux-android-
 
-make ARCH=arm64 exynos7420-zeroflte_defconfig
+make ARCH=arm64 exynos7420-zerolte_defconfig
 make ARCH=arm64
